29e0a3dfdef4e574a553da2376520427f435dcd5
nielsmadan <nielsmadan@gmail.com>
2017-05-26 16:02:55 +0200

    add option to change plugin prefix.

--- plugin/yankee_main.py
+++ plugin/yankee_main.py
@@ -1,12 +1,14 @@
 import venom
 import vim
 
+plugin_prefix = "<leader>%s" % (vim.g.yankee_prefix if "yankee_prefix" in vim.g else "y")
+
 from yankee import lock_paste_register, unlock_paste_register, toggle_lock_register
 
 venom.py_fn_to_vim_command("YankeeLockPasteRegister", lock_paste_register)
 venom.py_fn_to_vim_command("YankeeUnlockPasteRegister", unlock_paste_register)
 venom.py_fn_to_vim_command("YankeeToggleLockPasteRegister", toggle_lock_register)
 
-vim.map.nnoremap("<leader>yl", ":YankeeLockPasteRegister<CR>")
-vim.map.nnoremap("<leader>yu", ":YankeeUnlockPasteRegister<CR>")
-vim.map.nnoremap("<leader>yt", ":YankeeToggleLockPasteRegister<CR>")
+vim.map.nnoremap(plugin_prefix + "l", ":YankeeLockPasteRegister<CR>")
+vim.map.nnoremap(plugin_prefix + "u", ":YankeeUnlockPasteRegister<CR>")
+vim.map.nnoremap(plugin_prefix + "t", ":YankeeToggleLockPasteRegister<CR>")
